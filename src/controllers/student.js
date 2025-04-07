1cb3bd707c78db9e7d1b54d54c7d58957d211dfe
marcarro <hmarroyom@gmail.com>
2025-04-07 10:53:13 -0600

    lol

--- src/controllers/student.js
+++ src/controllers/student.js
@@ -4,7 +4,30 @@ class StudentController {
     }
   
     async getAll() {
-      return this.service.getAllStudents();
+      const students = await this.service.getAllStudents();
+      return students.map(student => {
+        let estatus;
+        
+        if (student.calificacion >= 70) {
+            if (student.deuda <= 0) {
+                estatus = "Aprobado";
+            } else {
+                estatus = "Reestructura";
+            }
+        } else {
+            if (student.deuda <= 0) {
+                estatus = "Pendiente";
+            } else {
+                estatus = "Expulsado";
+            }
+        }
+        
+        return {
+          matricula: student.matricula,
+          nombre: student.nombre,
+          estatus: estatus
+        };
+      });
     }
 }
   
